53880ef3ea28b924cb966d62de4a1ed560e964ad
mahsa <mahsa.ghaderan@gmail.com>
2022-08-06 01:34:15 +0430

    Upload Exam1

---
+++ E1/Q1/Q1/CandidateGenerator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    public static class CandidateGenerator
+    {
+        public static readonly char[] Alphabet =
+            Enumerable.Range('a', 'z' - 'a' + 1)
+                      .Select(c => (char)c)
+                      .ToArray();
+
+        public static string[] GetCandidates(string word)
+        {
+            List<string> candidates = new List<string>();
+            for(int i=0;i<word.Length;i++)
+            {
+                foreach(char alpha in Alphabet)
+                {
+                    candidates.Add(Insert(word, i, alpha));
+                    candidates.Add(Substitute(word, i, alpha));
+                }
+                candidates.Add(Delete(word, i));
+            }
+            foreach (char alpha in Alphabet)
+            {
+                candidates.Add(Insert(word, word.Length, alpha));
+            }
+            return candidates.ToArray();
+        }
+
+        private static string Insert(string word, int pos, char c)
+        {
+            char[] wordChars = word.ToCharArray();
+            char[] newWord = new char[wordChars.Length+1];
+            for (int i = 0; i < wordChars.Length +1; i++)
+            {
+                if (i == pos)
+                {
+                    newWord[i] = c;
+                }
+                else if(i> pos)
+                    newWord[i] = wordChars[i-1];
+                else
+                    newWord[i] = wordChars[i];
+            }
+            return new string(newWord);
+        }
+
+        private static string Delete(string word, int pos)
+        {
+            char[] wordChars = word.ToCharArray();
+            char[] newWord = new char[wordChars.Length-1];
+            for (int i = 0; i < wordChars.Length-1; i++)
+            {
+                if (i >= pos)
+                    newWord[i] = wordChars[i+1];
+                else
+                    newWord[i] = wordChars[i];
+
+            }
+            return new string(newWord);
+        }
+
+        private static string Substitute(string word, int pos, char c)
+        {
+            char[] wordChars = word.ToCharArray();
+            char[] newWord = new char[wordChars.Length];
+            for (int i = 0; i < wordChars.Length; i++)
+            {
+                if (i == pos)
+                    newWord[i] = c;
+                else
+                    newWord[i] = wordChars[i];
+            }
+            return new string(newWord);
+        }
+
+    }
+}
---
+++ E1/Q1/Q1/FastLM.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    public class FastLM
+    {
+        public readonly WordCount[] WordCounts;
+
+
+        public FastLM(WordCount[] wordCounts)
+        {
+            this.WordCounts = wordCounts.OrderBy(wc => wc.Word).ToArray();
+        }
+
+        public bool GetCount(string word, out ulong count)
+        {
+            count = 0;
+            long first=0, last=WordCounts.Length-1;
+            GetCount((int) first,(int)last, word,ref count);
+            if (count>0)
+                return true;
+            return false;
+        }
+
+        public void GetCount(int first,int last,string word,ref ulong count)
+        {
+            int index =(first + last) / 2;
+            int result = string.Compare(word, WordCounts[index].Word);
+            if (result==0)
+            {
+                count = WordCounts[index].Count;
+                return;
+            }
+            if (first >= last)
+                return;
+            else if (result < 0)
+                GetCount(first, index - 1, word, ref count);
+            else
+                GetCount(index + 1, last, word, ref count);
+
+        }
+        ///private iterator Count()
+    }
+}
