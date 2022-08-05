458b6b3f3812f2b3b0438593eb2b8759b5b5cb57
Mahsa Ghaderan <mahsa.ghaderan@gmail.com>
2022-08-05 16:33:23 +0430

    Add Test folder 8

---
+++ A8/A8Tests/GradedTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using A8;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A8.Tests
+{
+    [TestClass()]//Grade:A8:100
+    public class GradedTests
+    {
+        [TestMethod(),Timeout(2000)]
+        [DeploymentItem("TestData", "A8_TestData")]
+        public void SolveTest()
+        {
+            Processor[] problems = new Processor[] {
+
+                new PacketProcessing("TD3"),
+                new TreeHeight("TD2"),
+                new CheckBrackets("TD1")
+            };
+            
+            foreach (var p in problems)
+            {
+                TestTools.RunLocalTest("A8", p.Process, p.TestDataName);
+            }
+        }
+    }
+}
