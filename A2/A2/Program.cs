8d0d5bf459b800c52787ad2814d3e0e83da06b3f
Mahsa Ghaderan <mahsa.ghaderan@gmail.com>
2022-08-05 14:09:35 +0430

    Upload Assignment 2

---
+++ A2/A2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string Process(string input)
+        {
+            var inData = input.Split(new char[] { '\n', '\r', ' ' }
+                , StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => int.Parse(s))
+                .ToList();
+            return FastMaxPairwiseProduct(inData).ToString();
+        }
+
+        public static long NaiveMaxPairwiseProduct(List<int> numbers)
+        {
+            int max=0;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                for (int j = 0; j < numbers.Count; j++)
+                {
+                    if (i != j)
+                    {
+                        int zarb = numbers[i] * numbers[j];
+                        if (zarb > max)
+                            max = zarb;
+                    }
+                }
+            }
+            return max;
+        }
+
+        public static long FastMaxPairwiseProduct(List<int> numbers)
+        {
+            int index1=0;
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] > numbers[index1])
+                    index1 = i;
+            }
+            int index2;
+            if (index1 == 0)
+                index2 = 1;
+            else
+                index2 = 0;
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] > numbers[index2] && i != index1)
+                    index2 = i;
+            }
+            return (numbers[index1] * numbers[index2]);
+        }
+    }
+}
---
+++ A2/A2Tests/ProgramTests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using A2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+using System.Diagnostics;
+
+namespace A2.Tests
+{
+    [TestClass()]//Grade:A2:100
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void NaiveMaxPairwiseProductTest()
+        {
+            List<int> test1 = new List<int>(3);
+            test1.Add(1);
+            test1.Add(2);
+            test1.Add(3);
+            Assert.IsTrue(Program.NaiveMaxPairwiseProduct(test1) == 6);
+
+            List<int> test2 = new List<int>(3);
+            test2.Add(10);
+            test2.Add(12);
+            test2.Add(15);
+            test2.Add(20);
+            Assert.IsTrue(Program.NaiveMaxPairwiseProduct(test2) == 300);
+        }
+
+        [TestMethod(), Timeout(500)]
+        [DeploymentItem("TestData", "A2_TestData")]
+        public void GradedTest_Performance()
+        {
+            TestTools.RunLocalTest("A2",Program.Process);
+        }
+
+        [TestMethod()]
+        [DeploymentItem("TestData", "A2_TestData")]
+        public void GradedTest_Conrrectness()
+        {
+            TestTools.RunLocalTest("A2",Program.Process);
+        }
+
+        [TestMethod()]
+        public void GradedTest_Stress()
+        {
+            Stopwatch time = new Stopwatch();
+            Random rand = new Random();
+            time.Start();
+            while (time.ElapsedTicks < 5000)
+            {
+                int n = rand.Next() % 200+2;
+                int[] data = new int[n];
+                for (int i = 0; i < n; i++)
+                    data[i] = rand.Next() % 1000;
+                long naive = Program.NaiveMaxPairwiseProduct(data.ToList());
+                long fast = Program.FastMaxPairwiseProduct(data.ToList());
+                Assert.IsTrue(naive == fast);
+            }
+        }
+
+        [TestMethod()]
+        public void FastMaxPairwiseProductTest()
+        {
+            List<int> test1 = new List<int>(3);
+            test1.Add(1);
+            test1.Add(2);
+            test1.Add(3);
+            Assert.IsTrue(Program.FastMaxPairwiseProduct(test1) == 6);
+
+            List<int> test2 = new List<int>(3);
+            test2.Add(10);
+            test2.Add(12);
+            test2.Add(15);
+            test2.Add(20);
+            Assert.IsTrue(Program.FastMaxPairwiseProduct(test2) == 300);
+        }
+    }
+}
